cf17b68d82a2ac971259ecef1436efe5a55a7f11
Yan <codearth@outlook.fr>
2023-08-19 10:44:38 +0200

    basic floppy read

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -4,9 +4,9 @@ bits 16
 
 %define ENDL 0x0D 0x0A
 
-#
-# FAT 12 header
-#
+;
+; FAT 12 header
+;
 jmp short start  
 nop
 
@@ -24,7 +24,7 @@ bdb_heads: dw 2
 bdb_hidden_sectors: dd 0
 bdb_large_sector_count: dd 0
 
-# ebr
+; ebr
 ebr_drive_number: db 0
 db 0
 ebr_signature: db  29h
@@ -66,18 +66,99 @@ main:
     mov ss, ax
     mov sp, 0x7C00
 
+    mov [ebr_drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7C00
+
     ; finally print message
     mov si, msg_hello
     call puts
-
     hlt
 
-.halt:
-    jmp .halt
+floppy_error:
+    mov si, msg_read_failed
+    call puts
+    jmp wait_key_and_reboot
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0FFFFh:0
 
+.halt:
+    cli
+    hlt 
+
+
+
+; disk
+lba_to_chs:
+    push ax
+    push dx
+     
+    xor dx, dx
+    div word [bdb_sectors_per_track]
+    inc dx
+    mov cx, dx
+
+    xor dx, dx
+    div word [bdb_heads]
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al 
+    pop ax
+    ret
+
+disk_read:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+    push cx
+    call lba_to_chs
+    pop ax
+    mov ah, 02h
+    mov di, 3
+
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+    popa
+    call disk_reset
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp floppy_err
+
+.done:
+    popa
+    push di
+    push dx
+    push cx
+    push bx
+    push ax
+    ret
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa ret
 
 msg_hello: db "Hello world!", ENDL, 0
-
+msg_read_failed: db "Failed to read floppy disk !", ENDL, 0
 times 510-($-$$) db 0
 
 dw 0AA55h
